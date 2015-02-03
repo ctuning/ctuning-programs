613d718320c318fba3f2062c4706f1a929567ef9
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-02-03 22:15:16 +0100

    test for new CK auto-tuning

---
+++ program/benchmark-cbench-automotive-susan/_ck_compile.bat
@@ -0,0 +1,5 @@
+call env-mingw-gcc472-x86.bat
+
+rem gcc.exe *.c
+
+ck compile program
---
+++ program/benchmark-cbench-automotive-susan/_ck_run.bat
@@ -0,0 +1,4 @@
+rem call env-mvs2013-12-x64.bat
+call env-mingw-gcc472-x86.bat
+
+ck run program --cmd_key=edges
