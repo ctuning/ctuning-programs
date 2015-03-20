7a26d1b2f3547e3d890034afa16743ee894d5a20
Grigori Fursin <grigori.fursin@cTuning.org>
2015-03-20 17:09:04 +0100

    improving auto-tuning + adding 2D plot with variation

---
+++ program/benchmark-cbench-automotive-susan/_ck_plot_with_variation.bat
@@ -0,0 +1,1 @@
+ck plot experiment.graph: @_ck_plot_with_variation.json
--- program/benchmark-cbench-automotive-susan/ctuning-rtl.c
+++ program/benchmark-cbench-automotive-susan/ctuning-rtl.c
@@ -13,9 +13,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef OPENME
+#include <openme.h>
+#endif
+
 int main1(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-  return main1(argc, argv);
+  long ct_repeat=0;
+  long ct_repeat_max=1;
+  int ct_return=0;
+
+#ifdef OPENME
+  openme_init(NULL,NULL,NULL,0);
+  openme_callback("PROGRAM_START", NULL);
+#endif
+
+  if (getenv("CT_REPEAT_MAIN")!=NULL) ct_repeat_max=atol(getenv("CT_REPEAT_MAIN"));
+  			  
+#ifdef OPENME
+  openme_callback("KERNEL_START", NULL);
+#endif
+  for (ct_repeat=0; ct_repeat<ct_repeat_max; ct_repeat++)
+    ct_return=main1(argc, argv);
+#ifdef OPENME
+  openme_callback("KERNEL_END", NULL);
+#endif
+
+#ifdef OPENME
+  openme_callback("PROGRAM_END", NULL);
+#endif
+
+  return ct_return;
 }
